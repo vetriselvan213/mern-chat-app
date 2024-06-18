3525ded6191fc070d34fa8e6e8a3fbb40f2c898c
s vetriselvan <122189457+vetriselvan213@users.noreply.github.com>
2024-06-18 19:03:54 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://mern-chat-app-wdac.onrender.com"
+        target: "http://localhost:8000/"
       }
     }
   }
